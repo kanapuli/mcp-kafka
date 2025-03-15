2b641a4ada7181db283f8fa59e55d100d68648d7
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-03-15 16:53:50 +0100

    Register Tool to create kafka topic
    The tool can take the number of partitions and replication factors as
    arguments

--- handler.go
+++ handler.go
@@ -24,3 +24,22 @@ func (k *KafkaHandler) Register(ctx context.Context, args FuncArgs) (*mcp_golang
 
 	return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(fmt.Sprintf("Vanakam, %s. Content: %s. Topic: %s", args.Submitter, args.Content, args.Topic))), nil
 }
+
+func (k *KafkaHandler) CreateTopic(ctx context.Context, args FuncArgs) (*mcp_golang.ToolResponse, error) {
+
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	kafkaClient, err := kafka.NewClient(kafka.WithBootstrapServers([]string{"localhost:9092"}))
+	if err != nil {
+		return nil, err
+	}
+	defer kafkaClient.Close()
+
+	if err := kafkaClient.CreateTopic(args.Topic, args.NumPartitions, args.ReplicationFactor); err != nil {
+		return nil, err
+	}
+
+	return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(fmt.Sprintf("Creating topic %s", args.Topic))), nil
+}
--- main.go
+++ main.go
@@ -1,30 +1,54 @@
 package main
 
 import (
+	"context"
+	"os"
+	"time"
+
 	mcp_golang "github.com/metoro-io/mcp-golang"
 	"github.com/metoro-io/mcp-golang/transport/stdio"
+	"go.uber.org/zap"
 )
 
 type FuncArgs struct {
-	Submitter string `json:"submitter" jsonschema:"required,description=The name of the thing calling this tool."`
-	Content   string `json:"content" jsonschema:"required,description=The content of the message"`
-	Topic     string `json:"topic" jsonschema:"required,description=The topic which the kafka client will publish the message to"`
+	Submitter         string `json:"submitter" jsonschema:"required,description=The name of the thing calling this tool."`
+	Content           string `json:"content" jsonschema:"required,description=The content of the message"`
+	Topic             string `json:"topic" jsonschema:"required,description=The topic which the kafka client should work with"`
+	NumPartitions     int32  `json:"num_partitions" jsonschema:"required,description=The number of partitions for the topic"`
+	ReplicationFactor int16  `json:"replication_factor" jsonschema:"required,description=The replication factor for the topic"`
 }
 
 func main() {
 	done := make(chan struct{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
 	kafkaHandler := &KafkaHandler{}
 	// Todo: Add more description
 	err := server.RegisterTool("kafka", "Performs client operations on Kafka brokers", kafkaHandler.Register)
-
 	if err != nil {
-		panic(err)
+		zap.S().Errorf("error registering kafka mcp server: %v", err)
+		os.Exit(1)
 	}
 
-	err = server.Serve()
-	if err != nil {
-		panic(err)
+	go func(ctx context.Context) {
+		err := server.RegisterTool("create_topic", "Create a topic with the given number of partitions and replication factor", kafkaHandler.CreateTopic)
+		if err != nil {
+			zap.S().Errorf("error registering kafka topic resource: %v", err)
+		}
+
+		time.Sleep(1 * time.Second)
+
+		err = server.DeregisterResource("kafka://topic")
+		if err != nil {
+			zap.S().Errorf("error deregistering kafka topic resource: %v", err)
+		}
+	}(ctx)
+
+	if err = server.Serve(); err != nil {
+		zap.S().Errorf("error serving kafka mcp server: %v", err)
+		os.Exit(1)
 	}
 
 	<-done
