4dcb38d474be9e065e243e11f83e90383474056f
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-03-16 08:08:07 +0100

    Add DeleteTopic and ListTopics in topics.go

--- kafka/topic.go
+++ kafka/topic.go
@@ -15,3 +15,20 @@ func (c *client) CreateTopic(topic string, numPartitions int32, replicationFacto
 	}
 	return nil
 }
+
+// DeleteTopic deletes the specified topic.
+func (c *client) DeleteTopic(topic string) error {
+	if err := c.admin.DeleteTopic(topic); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListTopics lists all topics and their details.
+func (c *client) ListTopics() (map[string]sarama.TopicDetail, error) {
+	topics, err := c.admin.ListTopics()
+	if err != nil {
+		return nil, err
+	}
+	return topics, nil
+}
