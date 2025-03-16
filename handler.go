fb0da5ddae3816acef99241d122acdb6ca969f00
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-03-16 08:44:48 +0100

    Add DescribeTopic Handler

--- handler.go
+++ handler.go
@@ -81,3 +81,50 @@ func (k *KafkaHandler) ListTopics(ctx context.Context, req Request) (*mcp_golang
 
 	return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(response)), nil
 }
+
+// DescribeTopic describes the specified topic
+func (k *KafkaHandler) DescribeTopic(ctx context.Context, req Request) (*mcp_golang.ToolResponse, error) {
+
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	kafkaClient, err := kafka.NewClient(kafka.WithBootstrapServers([]string{"localhost:9092"}))
+	if err != nil {
+		return nil, err
+	}
+	defer kafkaClient.Close()
+
+	details, err := kafkaClient.DescribeTopic(req.Topic)
+	if err != nil {
+		return nil, err
+	}
+
+	response := fmt.Sprintf("Hey %s, Format the following details in a nice table format or in a json format\n", req.Submitter)
+	for _, d := range details {
+		response += fmt.Sprintf("Topic Name: %s\n", d.Name)
+		if d.Err != 0 {
+			response += fmt.Sprintf("Topic Error: %s\n", d.Err)
+		}
+
+		switch d.IsInternal {
+		case true:
+			response += fmt.Sprintf("Topic is an internal\n")
+		case false:
+			response += fmt.Sprintf("Topic is not internal\n")
+		}
+
+		response += fmt.Sprintf("## Topic Partition Details")
+		for i, partition := range d.Partitions {
+			response += fmt.Sprintf("%d) Partition ID: %d\n", i+1, partition.ID)
+			response += fmt.Sprintf("Partition Leader Broker ID: %d\n", partition.Leader)
+			response += fmt.Sprintf("Partition Leader Epoch: %d\n", partition.LeaderEpoch)
+			response += fmt.Sprintf("Partition Replicas: %v\n", partition.Replicas)
+			response += fmt.Sprintf("Partition Errors: %v\n", partition.Err)
+			response += fmt.Sprintf("Insync replicas: %v\n", partition.Isr)
+			response += fmt.Sprintf("Offline Replicas: %v\n", partition.OfflineReplicas)
+		}
+	}
+
+	return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(response)), nil
+}
