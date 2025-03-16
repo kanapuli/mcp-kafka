ea18d13f48e4f74066c9bd23c6afcb9d7298ba19
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-03-16 09:51:14 +0100

    Remove WithProducerTopic method

--- kafka/options.go
+++ kafka/options.go
@@ -33,17 +33,6 @@ func WithPassword(password string) kafkaOptions {
 	}
 }
 
-// WithProducerTopic sets the topic for the producer.
-func WithProducerTopic(topic string) kafkaOptions {
-	return func(c *Client) error {
-		if topic == "" {
-			return errors.New("topic cannot be empty")
-		}
-		c.producer.topic = topic
-		return nil
-	}
-}
-
 // WithVerbose sets the verbose flag for the client.
 func WithVerbose(verbose bool) kafkaOptions {
 	return func(c *Client) error {
